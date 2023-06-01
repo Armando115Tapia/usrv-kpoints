366606ae5184faa6e3d7700547840670c8a19560
Armando Tapia <jusnfds123@hotmail.com>
2023-06-01 10:48:37 -0500

    Init commit

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,11 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log("Los logs")
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: "Go Serverless v1.0! Your function executed successfully! Jair",
         input: event,
       },
       null,
